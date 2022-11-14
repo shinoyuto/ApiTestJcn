f004bc6ca42ea2a3a2e5665a777a2a5c000f75bd
IFportalApiTest <114624541+IFportalApiTest@users.noreply.github.com>
2022-11-14 16:56:50 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+const _ = require('lodash');
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
 // In this example we simulate a server side error response
 router.render = (req, res) => {
   res.status(503).jsonp({
